c4888f99e513268dba3bdb39d0dbc724a3463169
Kneelawk <kneelawk@gmail.com>
2023-05-13 21:21:54 -0700

    Setup multi-versioning

---
+++ Common/src/main/java/com/kneelawk/aquifersbegone/Constants.java
@@ -0,0 +1,11 @@
+package com.kneelawk.aquifersbegone;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Constants {
+
+    public static final String MOD_ID = "aquifersbegone";
+    public static final String MOD_NAME = "Aquifers Be Gone";
+    public static final Logger LOG = LoggerFactory.getLogger(MOD_NAME);
+}
---
+++ Forge-1.19.2/src/main/java/com/kneelawk/aquifersbegone/platform/ForgePlatformHelper.java
@@ -0,0 +1,27 @@
+package com.kneelawk.aquifersbegone.platform;
+
+import net.minecraftforge.fml.ModList;
+import net.minecraftforge.fml.loading.FMLLoader;
+
+import com.kneelawk.aquifersbegone.platform.services.IPlatformHelper;
+
+public class ForgePlatformHelper implements IPlatformHelper {
+
+    @Override
+    public String getPlatformName() {
+
+        return "Forge";
+    }
+
+    @Override
+    public boolean isModLoaded(String modId) {
+
+        return ModList.get().isLoaded(modId);
+    }
+
+    @Override
+    public boolean isDevelopmentEnvironment() {
+
+        return !FMLLoader.isProduction();
+    }
+}
