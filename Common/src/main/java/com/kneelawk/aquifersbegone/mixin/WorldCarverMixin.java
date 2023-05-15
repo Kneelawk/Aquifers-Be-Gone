63f163948f89ecb34ac38d2834bf47bccb29c8c3
Kneelawk <kneelawk@gmail.com>
2023-05-14 21:07:44 -0700

    Preserve cave fanciness

--- Common/src/main/java/com/kneelawk/aquifersbegone/mixin/WorldCarverMixin.java
+++ Common/src/main/java/com/kneelawk/aquifersbegone/mixin/WorldCarverMixin.java
@@ -2,6 +2,8 @@ package com.kneelawk.aquifersbegone.mixin;
 
 import java.util.function.Function;
 
+import javax.annotation.Nullable;
+
 import org.apache.commons.lang3.mutable.MutableBoolean;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
@@ -11,9 +13,12 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
 import net.minecraft.core.Holder;
 import net.minecraft.world.level.biome.Biome;
+import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.properties.BlockStateProperties;
 import net.minecraft.world.level.chunk.CarvingMask;
 import net.minecraft.world.level.chunk.ChunkAccess;
 import net.minecraft.world.level.levelgen.Aquifer;
@@ -29,8 +34,12 @@ public abstract class WorldCarverMixin {
     protected abstract boolean canReplaceBlock(CarverConfiguration $$0, BlockState $$1);
 
     @Shadow
+    @Nullable
+    protected abstract BlockState getCarveState(CarvingContext $$0, CarverConfiguration $$1, BlockPos $$2, Aquifer $$3);
+
+    @Shadow
     @Final
-    protected static BlockState CAVE_AIR;
+    protected static BlockState AIR;
 
     @Inject(method = "carveBlock", at = @At("HEAD"), cancellable = true)
     private void onCarveBlock(CarvingContext ctx, CarverConfiguration config, ChunkAccess chunk,
@@ -38,8 +47,36 @@ public abstract class WorldCarverMixin {
                               BlockPos.MutableBlockPos pos, BlockPos.MutableBlockPos downPos, Aquifer aquifer,
                               MutableBoolean foundSurface, CallbackInfoReturnable<Boolean> cir) {
         if (((Object) this) instanceof CaveWorldCarver || ((Object) this) instanceof CanyonWorldCarver) {
+            // water-less version of cave carve-block method
+
             if (canReplaceBlock(config, chunk.getBlockState(pos)) && chunk.getFluidState(pos).isEmpty()) {
-                chunk.setBlockState(pos, CAVE_AIR, false);
+                BlockState state = getCarveState(ctx, config, pos, aquifer);
+
+                if (state == null) {
+                    cir.setReturnValue(false);
+                    return;
+                }
+
+                if (!state.getFluidState().isEmpty()) {
+                    BlockState newState = state.trySetValue(BlockStateProperties.WATERLOGGED, false);
+                    if (newState == state) {
+                        state = AIR;
+                    } else {
+                        state = newState;
+                    }
+                }
+
+                chunk.setBlockState(pos, state, false);
+
+                if (foundSurface.isTrue()) {
+                    downPos.setWithOffset(pos, Direction.DOWN);
+
+                    if (chunk.getBlockState(downPos).is(Blocks.DIRT)) {
+                        ctx.topMaterial(posToBiome, chunk, pos, false)
+                            .ifPresent(topMaterial -> chunk.setBlockState(downPos, topMaterial, false));
+                    }
+                }
+
                 cir.setReturnValue(true);
             } else {
                 cir.setReturnValue(false);
